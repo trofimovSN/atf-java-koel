ce3affc4cdd1cf38d4dba71d39cef76c9064d05a
sRgio-sudo <sergei.trofimov@testpro.io>
2026-02-18 09:31:21 -0500

    Add pickBrowser method

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,7 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -14,6 +20,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import utils.ConfigReader;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 import java.util.UUID;
 
@@ -29,7 +37,7 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public void launchBrowser() {
+    public void launchBrowser() throws MalformedURLException {
         String url = ConfigReader.getProperty("base.url");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
@@ -37,7 +45,11 @@ public class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-infobars");
 
-        driver = new ChromeDriver(options);
+        String browser = System.getProperty("browser");
+        if (browser == null) {
+            browser = "chrome";
+        }
+        driver = pickBrowser(browser);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         actions = new Actions(driver);
@@ -145,4 +157,43 @@ public class BaseTest {
                 By.xpath("//a[contains(text(),'" + playListName + "')]")));
         actions.doubleClick(playListRenamer).perform();
     }
+
+    public WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://localhost:4444";
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "MicrosoftEdge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--remote-all-origins=*");
+                return driver = new EdgeDriver();
+            case "grid-edge":
+                caps.setCapability("browserName", "MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-ie":
+                InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+                ieOptions.introduceFlakinessByIgnoringSecurityDomains();
+                ieOptions.ignoreZoomSettings();
+                driver = new RemoteWebDriver(URI.create(gridURL).toURL(), ieOptions);
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                chromeOptions.addArguments("--start-maximized");
+                chromeOptions.addArguments("--disable-notifications");
+                chromeOptions.addArguments("--disable-infobars");
+
+                return driver = new ChromeDriver(chromeOptions);
+        }
+    }
 }
+
